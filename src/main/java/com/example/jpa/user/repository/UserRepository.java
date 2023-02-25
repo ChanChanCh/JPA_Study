4d98ec5d665d4861cabafcbd7db5300329030d58
DESKTOP-OHPTNOM\user <mn99108@naver.com>
2023-02-25 12:48:24 +0900

    [feat] Q32번문제 ####32 사용자 정보를 입력받아서 저장하는 API를 작성해 보세요.
    입력값: 이메일(유일한 값 확인), 이름, 비밀번호, 연락처, 가입일(현재일시)
    
    yml jpa부분 변경
    
    User 생성
    UserRepository 생성

---
+++ src/main/java/com/example/jpa/user/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.example.jpa.user.repository;
+
+import com.example.jpa.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+
+}
